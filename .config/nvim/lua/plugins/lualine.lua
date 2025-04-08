ce89bd77601fed66906302da7d02594f70fbdf4c
Santos Vilanculos <95357132+SantosVilanculos@users.noreply.github.com>
2025-04-08 03:55:35 +0200

    chore(nvim): lualine_z show the current project name

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -8,7 +8,17 @@ return {
         component_separators = "",
       },
       sections = {
-        lualine_z = {},
+        lualine_z = {
+          {
+            function()
+              return vim.fn.fnamemodify(vim.fn.getcwd(), ":t")
+            end,
+            color = { fg = "#000000", bg = "#fdba74" },
+            cond = function()
+              return vim.fn.getcwd() ~= ""
+            end,
+          },
+        },
       },
     },
   },
