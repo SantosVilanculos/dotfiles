dec565bc4b9cef07eb6f1e3c00ab9edb3478b8a1
Santos Vilanculos <95357132+SantosVilanculos@users.noreply.github.com>
2025-04-28 12:08:19 +0200

    chore(nvim): auto set lualine_z fg and bg color

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -13,7 +13,6 @@ return {
             function()
               return vim.fn.fnamemodify(vim.fn.getcwd(), ":t")
             end,
-            color = { fg = "#000000", bg = "#fdba74" },
             cond = function()
               return vim.fn.getcwd() ~= ""
             end,
