d33eec7af8830eb4731f47f8a7e1c481cdbf133f
Santos Vilanculos <santosvilanculos@yahoo.com>
2025-06-25 15:08:16 +0200

    chore(nvim): add min theme and update pluguins

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -25,9 +25,25 @@ return {
     -- github_light_tritanopia
   },
   {
+    "datsfilipe/min-theme.nvim",
+    priority = 1000,
+    lazy = false,
+    -- (note: if your configuration sets vim.o.background the following option will do nothing!)
+    theme = "dark", -- String: 'dark' or 'light', determines the colorscheme used
+    transparent = false, -- Boolean: Sets the background to transparent
+    italics = {
+      comments = true, -- Boolean: Italicizes comments
+      keywords = true, -- Boolean: Italicizes keywords
+      functions = true, -- Boolean: Italicizes functions
+      strings = true, -- Boolean: Italicizes strings
+      variables = true, -- Boolean: Italicizes variables
+    },
+    overrides = {},
+  },
+  {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "onedark",
+      colorscheme = "min-theme",
     },
   },
 }
