6754b0dcefcc397a59f2caf17af937cd52ef521f
Santos Vilanculos <santosvilanculos@yahoo.com>
2025-09-05 21:24:19 +0200

    fix(nvim): update colors cheme config

--- configuration/.config/nvim/lua/plugins/colorscheme.lua
+++ configuration/.config/nvim/lua/plugins/colorscheme.lua
@@ -26,23 +26,25 @@ return {
     "datsfilipe/min-theme.nvim",
     -- priority = 1000,
     -- lazy = false,
-    -- (note: if your configuration sets vim.o.background the following option will do nothing!)
-    theme = "dark", -- String: 'dark' or 'light', determines the colorscheme used
-    transparent = false, -- Boolean: Sets the background to transparent
-    italics = {
-      comments = true, -- Boolean: Italicizes comments
-      keywords = true, -- Boolean: Italicizes keywords
-      functions = true, -- Boolean: Italicizes functions
-      strings = true, -- Boolean: Italicizes strings
-      variables = true, -- Boolean: Italicizes variables
+    opts = {
+      -- (note: if your configuration sets vim.o.background the following option will do nothing!)
+      theme = "dark", -- String: 'dark' or 'light', determines the colorscheme used
+      transparent = false, -- Boolean: Sets the background to transparent
+      italics = {
+        comments = true, -- Boolean: Italicizes comments
+        keywords = true, -- Boolean: Italicizes keywords
+        functions = true, -- Boolean: Italicizes functions
+        strings = true, -- Boolean: Italicizes strings
+        variables = true, -- Boolean: Italicizes variables
+      },
+      overrides = {},
     },
-    overrides = {},
   },
   {
     "rose-pine/neovim",
     name = "rose-pine",
-    priority = 1000,
-    lazy = false,
+    -- priority = 1000,
+    -- lazy = false,
     opts = {
       variant = "auto", -- auto, main, moon, or dawn
       dark_variant = "main", -- main, moon, or dawn
@@ -56,16 +58,18 @@ return {
     "datsfilipe/vesper.nvim",
     priority = 1000,
     lazy = false,
-    transparent = false, -- Boolean: Sets the background to transparent
-    italics = {
-      comments = true, -- Boolean: Italicizes comments
-      keywords = true, -- Boolean: Italicizes keywords
-      functions = true, -- Boolean: Italicizes functions
-      strings = true, -- Boolean: Italicizes strings
-      variables = true, -- Boolean: Italicizes variables
+    opts = {
+      transparent = false, -- Boolean: Sets the background to transparent
+      italics = {
+        comments = true, -- Boolean: Italicizes comments
+        keywords = true, -- Boolean: Italicizes keywords
+        functions = true, -- Boolean: Italicizes functions
+        strings = true, -- Boolean: Italicizes strings
+        variables = true, -- Boolean: Italicizes variables
+      },
+      overrides = {}, -- A dictionary of group names, can be a function returning a dictionary or a table.
+      palette_overrides = {},
     },
-    overrides = {}, -- A dictionary of group names, can be a function returning a dictionary or a table.
-    palette_overrides = {},
     -- vesper
   },
   {
