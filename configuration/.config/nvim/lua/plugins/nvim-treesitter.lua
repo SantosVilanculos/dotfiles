7e8e25195b7201e848ea4e32a0d0455f867e4643
Santos Vilanculos <santosvilanculos@yahoo.com>
2025-09-23 12:10:46 +0200

    wip

--- configuration/.config/nvim/lua/plugins/nvim-treesitter.lua
+++ configuration/.config/nvim/lua/plugins/nvim-treesitter.lua
@@ -42,27 +42,27 @@ return {
         enable_close_on_slash = false,
       }
     end,
-    config = function(_, opts)
-      local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-      parser_config.blade = {
-        install_info = {
-          url = "https://github.com/EmranMR/tree-sitter-blade",
-          files = { "src/parser.c" },
-          branch = "main",
-        },
-        generate_requires_npm = true,
-        requires_generate_from_grammar = true,
-        filetype = "blade",
-      }
-
-      vim.filetype.add({
-        pattern = {
-          [".*%.blade%.php"] = "blade",
-        },
-      })
-
-      require("nvim-treesitter.configs").setup(opts)
-    end,
+    -- config = function(_, opts)
+    --   local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+    --   parser_config.blade = {
+    --     install_info = {
+    --       url = "https://github.com/EmranMR/tree-sitter-blade",
+    --       files = { "src/parser.c" },
+    --       branch = "main",
+    --     },
+    --     generate_requires_npm = true,
+    --     requires_generate_from_grammar = true,
+    --     filetype = "blade",
+    --   }
+    --
+    --   vim.filetype.add({
+    --     pattern = {
+    --       [".*%.blade%.php"] = "blade",
+    --     },
+    --   })
+    --
+    --   require("nvim-treesitter.configs").setup(opts)
+    -- end,
   },
   {
     "nvim-treesitter/nvim-treesitter-context",
