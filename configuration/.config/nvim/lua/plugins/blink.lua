7dba7c3f07c5408457414d97b1411dbdff05bd80
Santos Vilanculos <santosvilanculos@yahoo.com>
2025-10-27 19:41:09 +0200

    chore(nvim): update blink config

--- configuration/.config/nvim/lua/plugins/blink.lua
+++ configuration/.config/nvim/lua/plugins/blink.lua
@@ -2,18 +2,13 @@ return {
   {
     "saghen/blink.cmp",
     version = "1.*",
-    build = "cargo build --release",
+    -- build = "cargo build --release",
     dependencies = { "rafamadriz/friendly-snippets" },
     ---@module 'blink.cmp'
     ---@type blink.cmp.Config
     opts = {
       keymap = {
         preset = "super-tab",
-        --   ["<Tab>"] = {
-        --     require("blink.cmp.keymap.presets").get("super-tab")["<Tab>"][1],
-        --     require("lazyvim.util.cmp").map({ "snippet_forward", "ai_accept" }),
-        --     "fallback",
-        --   },
       },
       completion = {
         menu = {
